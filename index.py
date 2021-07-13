afab0a90b2bce89c2c01bea4319249b617925daa
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-07-14 00:52:04 +0900

    coudlntSolvePagination

--- index.py
+++ index.py
@@ -5,8 +5,12 @@ from selenium.webdriver.chrome.options import Options
 
 driver = webdriver.Chrome('C:/Users/JYW/Desktop/chromedriver/chromedriver.exe')
 phantom = webdriver.PhantomJS(r'/Users/JYW/Desktop/phantomjs/phantomjs.exe')
-
 driver.implicitly_wait(3)
+
+
 driver.get('https://www.rocketpunch.com/jobs?career_type=1&job=1&page=1')
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
+
+pagination = soup.find_all("div", {"class":"pagination"})
+print(pagination)
