6b5d334bdc2b3fedc2555a388fe4c6d266362de1
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-07-14 17:55:15 +0900

    right before apply link

--- index.py
+++ index.py
@@ -22,12 +22,17 @@ def extract_jobs(last_page):
     for page in range(1, last_page + 1):
         print(f"Scraping Rocket-Punch Jobs for Junior: page: {page}")
         driver.get(f'https://www.rocketpunch.com/jobs?career_type=1&job=1&page={page}')
-        company_items = soup.select('div.company.item')
+        company_items = soup.select('div#company-list > div.company.item')
+        for company_item in company_items:
+            company = company_item.select_one('div.company-name > a > h4.name > strong').get_text()
+            job = company_item.select_one('div.job-detail > div > a.job-title').get_text()
+            career_info = company_item.select_one('span.job-stat').get_text()
+            apply_link = company_item.select_one('div.job-detail > div > a')
+        '''
         for company_item in company_items:
             company = soup.select_one('div.company-name > a > h4.name > strong').get_text()
             job = soup.select_one('div.job-detail > div > a.job-title').get_text()
             print(company, job)
-        '''
         companies = soup.select('div.company-name > a > h4.name > strong')
         for company in companies:
             company = company.get_text()
