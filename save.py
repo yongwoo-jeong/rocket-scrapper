31b57c3f5c6d08457e373005117ce8f06224720b
yongwoo-jeong <jywoo9@gmail.com>
2022-02-03 15:07:15 +0900

    modified div.class name of site

--- save.py
+++ save.py
@@ -4,7 +4,7 @@ from datetime import datetime
 today = datetime.today().strftime("%Y%m%d")
 
 def save_to_file(jobs):
-    file = open(f"로켓펀치_신입{today}.csv", mode = 'w', encoding ='UTF-8')
+    file = open(f"로켓펀치_신입{today}.csv", mode = 'w', encoding ='UTF-8-sig')
     writer = csv.writer(file)
     writer.writerow(["회사명", "직무", "연봉/경력", "지원"])
     for job in jobs:
